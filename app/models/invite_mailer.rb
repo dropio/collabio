cbc7a40db65f715649e237faaa391d050e70a2ad
Jake Good <jake@dropio.com>
2008-11-05 09:46:04 -0600

    Changed the message slightly

--- app/models/invite_mailer.rb
+++ app/models/invite_mailer.rb
@@ -1,8 +1,9 @@
 class InviteMailer < ActionMailer::Base
   
   def message(drop, coordinator, topic, time, emails, message)
+    emails << coordinator
     recipients emails
-    from coordinator
+    from "no-reply@collab.io"
     subject "Let's collaborate about #{topic} on #{time.strftime("%m/%d/%Y at %I:%M%p")}"
     body :drop => drop, :coordinator => coordinator, :topic => topic, :time => time, :message => message
     content_type "text/html"
